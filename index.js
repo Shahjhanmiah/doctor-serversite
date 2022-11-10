59d6055e92925da6bb4ad166ed118835e49e65fd
mdshahjhan <mdshahjha478877@gmail.com>
2022-11-10 14:41:20 +0600

    jwt

--- index.js
+++ index.js
@@ -11,15 +11,25 @@ const port = process.env.PORT||5000;
 app.use(cors())
 app.use(express.json())
 
-
-
-
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ga6ydds.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const servicesCollection=client.db('doctormongodb').collection('services')
 const orderCollection=client.db('doctormongodb').collection('orders')
 
-
+function verifyjWT(req,res,next){
+    const authorization = req.headers.authorization;
+    if(!authorization){
+       return res.status(401).send({message:'unauthorize access'})
+    }
+    const token = authorization.split('')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+        if(err){
+           return res.status(403).send({message:'unauthourize access'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 async function run(){
     client.connect()
     console.log("database connect");
@@ -31,21 +41,21 @@ async function run(){
             const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
             res.send({token})
         })
-        app.get('/Service',async(req,res)=>{
+        app.get('/Servicehome',async(req,res)=>{
             const query = {};
             const cursor = servicesCollection.find(query);
             const service= await cursor.limit(3).toArray();
             res.send(service)
         })
-        app.get('/service',async(req,res)=>{
+        app.get('/services',async(req,res)=>{
             const query = {};
             const cursor = servicesCollection.find(query);
-            const servic= await cursor.limit(6).toArray();
+            const servic= await cursor.toArray();
             res.send(servic)
         })
 
         // speack  id 
-        app.get('/Service/:id',async(req,res)=>{
+        app.get('/services/:id',async(req,res)=>{
             const id = req.params.id;
             const query={_id:ObjectId(id)}
             const service = await servicesCollection.findOne(query)
@@ -54,7 +64,13 @@ async function run(){
 
 
         // orders api 
-        app.get('/Orders',async(req,res)=>{
+        app.get('/Orders',verifyjWT,async(req,res)=>{
+            const decoded = req.decoded;
+            console.log("inside orders api",decoded);
+            if(decoded.email!==req.query.email){
+                 return res.status(403).send({message:'unauthorized'})
+            }
+         console.log(req.headers.authorization)
             let query = {};
              if(req.query.email){
                 query={
